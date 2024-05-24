66f513942615a4295a59a522b088dbc5869acaa2
dimitri <mamchenko2210@gmail.com>
2024-05-24 21:05:06 +0200

    init commit

---
+++ src/test/java/com/dblab/opensourcedata/OpensourcedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dblab.opensourcedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensourcedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
